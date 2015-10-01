bc5e82a101e65667b37104dcff5a1f9b67154403
MrPowers <matthewkevinpowers@gmail.com>
2015-10-01 15:41:30 -0500

    Use a better database name

--- spec/cmap/graph_to_sql_spec.rb
+++ spec/cmap/graph_to_sql_spec.rb
@@ -6,7 +6,7 @@ module Cmap; describe GraphToSql do
     it "runs the queries in the right order" do
       propositions_path = File.expand_path("../support/human_lab_data_propositions.txt", File.dirname(__FILE__))
       prop = PropositionsToGraph.new(propositions_path)
-      to_sql = GraphToSql.new("human_lab_data", prop.graph, {dbname: "messing_around"})
+      to_sql = GraphToSql.new("human_lab_data", prop.graph, {dbname: "cmap_test"})
 
       to_sql.connection.exec("DROP TABLE IF EXISTS human_lab_data;")
       to_sql.connection.exec("CREATE TABLE human_lab_data ( age integer, nationality text );")
@@ -18,7 +18,7 @@ module Cmap; describe GraphToSql do
     it "runs complicated queries in the right order" do
       propositions_path = File.expand_path("../support/complicated_propositions.txt", File.dirname(__FILE__))
       prop = PropositionsToGraph.new(propositions_path)
-      to_sql = GraphToSql.new("human_lab_data", prop.graph, {dbname: "messing_around"})
+      to_sql = GraphToSql.new("human_lab_data", prop.graph, {dbname: "cmap_test"})
 
       to_sql.connection.exec("DROP TABLE IF EXISTS human_lab_data;")
       to_sql.connection.exec("CREATE TABLE human_lab_data ( age integer, nationality text );")
