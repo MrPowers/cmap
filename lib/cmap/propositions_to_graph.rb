629031e630507de09c2a41c51b99c3e419ee14c4
MrPowers <matthewkevinpowers@gmail.com>
2015-10-08 11:31:33 -0500

    Make gsub the edge values based on the vertex values

--- lib/cmap/propositions_to_graph.rb
+++ lib/cmap/propositions_to_graph.rb
@@ -18,12 +18,34 @@ module Cmap; class PropositionsToGraph
     CSV.read(csv_path, { :col_sep => "\t", :quote_char => '"' })
   end
 
+  # vertices from the text file that haven't been gsubbed
+  def raw_vertices
+    propositions.inject([]) do |m, (origin_vertex, _, destination_vertex)|
+      m.push(origin_vertex, destination_vertex)
+      m
+    end.uniq
+  end
+
+  def sanitize_string(string)
+    string.gsub(/[^0-9a-zA-Z]+/, '_').downcase
+  end
+
+  def gsubbed_value(value)
+    raw_vertices.each do |raw_vertex|
+      if value.include?(raw_vertex)
+        value = value.gsub(raw_vertex, sanitize_string(raw_vertex))
+      end
+    end
+    value
+  end
+
   def edges
     @edges ||= propositions.inject([]) do |memo, e|
       origin_vertex, value, destination_vertex = e
       o = gsubber(vertex_gsubs, origin_vertex)
       d = gsubber(vertex_gsubs, destination_vertex)
-      memo << DirectedGraph::Edge.new(o, d, value)
+      v = gsubbed_value(value)
+      memo << DirectedGraph::Edge.new(o, d, v)
       memo
     end
   end
