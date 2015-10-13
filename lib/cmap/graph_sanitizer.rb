fa069623305def0a7fa333d0ee950382af0899d9
Evan Closson <eclosson@medivo.com>
2015-10-13 16:24:09 -0400

    [https://trello.com/c/pEhGRPK0] Added in a two pass sanitization process where first the vertices and then the edges are santized. The edges are dependenty upon the vertex sanitization hence they need to happen after vertex sanitzation. Also the vertex sanitization is applied in the order of more complex to less complex to account for 'reuse' of content within Humanized names. For example: 'IGF-1 Result, Male = 1' reuses the 'IGF-1 Result' Humanized name within it.

--- lib/cmap/graph_sanitizer.rb
+++ lib/cmap/graph_sanitizer.rb
@@ -4,23 +4,35 @@ module Cmap; class GraphSanitizer
 
   def initialize(graph)
     @graph = graph
+    @sanitized_vertex_lookup = {}
   end
 
   def sanitized_graph
     DirectedGraph::Graph.new(sanitized_edges)
   end
 
-  def sanitized_data
+  def edges_with_sanitized_vertices
     graph.edges.map do |edge|
       origin_vertex = sanitize_string(edge.origin_vertex)
-      d = sanitize_string(edge.destination_vertex)
-      {origin_vertex: origin_vertex, destination_vertex: d, value: edge.value}
+      destination_vertex = sanitize_string(edge.destination_vertex)
+      
+      @sanitized_vertex_lookup[edge.origin_vertex] = origin_vertex
+      @sanitized_vertex_lookup[edge.destination_vertex] = destination_vertex
+      
+      {origin_vertex: origin_vertex, destination_vertex: destination_vertex, value: edge.value}
     end
   end
 
+  def sanitize_edge_value(edge_args)
+    @sanitized_vertex_lookup.keys.sort.reverse.each do |original_vertex_name|
+      edge_args[:value].gsub!(original_vertex_name, @sanitized_vertex_lookup[original_vertex_name])
+    end
+    edge_args
+  end
+
   def sanitized_edges
-    sanitized_data.map do |args|
-      DirectedGraph::Edge.new(args)
+    edges_with_sanitized_vertices.map do |edge_args|
+      DirectedGraph::Edge.new(sanitize_edge_value(edge_args))
     end
   end
 
