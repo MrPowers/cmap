8063a5dbd68e5a8fc6681112a63e95dd6e4b2110
Rex Feng <rexfeng@gmail.com>
2016-01-13 14:32:39 -0800

    remove unused spec files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,2 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cmap'
-
-require File.expand_path("./support/helpers.rb", File.dirname(__FILE__))
-
-RSpec.configure do |c|
-  c.include Helpers
-end
